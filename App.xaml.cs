09cfb9ddd420c5b4dfd16608d566037dd0b47d5e
Alexandre Gregoire <agregoire.mg@icloud.com>
2019-06-21 02:09:19 -0400

    First commit.

---
+++ App.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+
+namespace unity_theme_switcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AdminRelauncher();
+            ExtractAllResources();
+
+            Properties.Add("CatPath", string.Concat(Path.GetTempPath(), "cat.exe"));
+            Properties.Add("DDPath", string.Concat(Path.GetTempPath(), "dd.exe"));
+            Properties.Add("PrintfPath", string.Concat(Path.GetTempPath(), "printf.exe"));
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            RemoveAllResources();
+        }
+
+        private void AdminRelauncher()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Assembly.GetEntryAssembly().CodeBase;
+
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                    Application.Current.Shutdown();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("This program must be run as an administrator! \n\n" + ex.ToString());
+                }
+            }
+        }
+
+        private bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void ExtractResource(string embeddedFileName, string destinationPath)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            string[] arrResources = currentAssembly.GetManifestResourceNames();
+            foreach (string resourceName in arrResources)
+            {
+                if (resourceName.ToUpper().EndsWith(embeddedFileName.ToUpper()))
+                {
+                    Stream resourceToSave = currentAssembly.GetManifestResourceStream(resourceName);
+                    string file = string.Concat(destinationPath, embeddedFileName);
+                    File.Create(string.Concat(destinationPath, embeddedFileName)).Close();
+                    var output = File.OpenWrite(file);
+                    resourceToSave.CopyTo(output);
+                    resourceToSave.Close();
+                }
+            }
+        }
+
+        private void ExtractAllResources()
+        {
+            string path = Path.GetTempPath();
+
+            ExtractResource("cat.exe", path);
+            ExtractResource("dd.exe", path);
+            ExtractResource("printf.exe", path);
+        }
+
+        private void RemoveAllResources()
+        {
+            string path = Path.GetTempPath();
+
+            File.Delete(string.Concat(path, "cat.exe"));
+            File.Delete(string.Concat(path, "dd.exe"));
+            File.Delete(string.Concat(path, "printf.exe"));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace unity_theme_switcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum Theme
+        {
+            NotSupported,
+            Dark,
+            Light
+        }
+
+        string exePath;
+        string unityVersion;
+        Theme currentTheme;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Select_Executable(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new CommonOpenFileDialog())
+            {
+                dialog.IsFolderPicker = false;
+
+                CommonFileDialogResult result = dialog.ShowDialog(this);
+
+                if (result == CommonFileDialogResult.Ok)
+                {
+                    // Minimal protection
+                    if(!dialog.FileName.ToLower().Contains("unity.exe"))
+                    {
+                        MessageBox.Show("This does not seem to be a Unity executable.", "Unity Theme Switcher", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+                    else if(IsUnityRunning())
+                    {
+                        MessageBox.Show("Please shutdown your Unity Editor before proceeding.", "Unity Theme Switcher", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+
+                    exePath = dialog.FileName;
+
+                    if (!string.IsNullOrEmpty(exePath))
+                    {                        
+                        unityVersion = FileVersionInfo.GetVersionInfo(exePath).ProductVersion;
+
+                        if (!string.IsNullOrEmpty(unityVersion))
+                        {
+                            DetectedVersionText.IsEnabled = true;
+                            UnityVersion.Text = unityVersion;
+
+                            currentTheme = GetCurrentTheme();
+
+                            SelectedFileText.IsEnabled = true;
+                            FolderLabel.Text = exePath;
+
+                            SwitchBox.IsEnabled = true;
+                            RefreshThemeUI();
+                            Toggle.IsEnabled = true;
+                            Toggle.FontSize = 16;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Switch_Theme(object sender, RoutedEventArgs e)
+        {
+            if (IsUnityRunning())
+            {
+                MessageBox.Show("Please shutdown your Unity Editor before proceeding.", "Unity Theme Switcher", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            BackupExecutable();
+
+            if (unityVersion.StartsWith("2019"))
+            {
+                string newByte = currentTheme == Theme.Dark ? "t" : "u";
+                string cmd = App.Current.Properties["PrintfPath"] + " " + newByte + " | " + App.Current.Properties["DDPath"] + " of=\"" + exePath + "\" bs=1 seek=16519510 count=1 conv=notrunc";
+                Process p = CreateConsoleProcess(cmd);
+                p.Start();
+                p.WaitForExit();
+            }
+
+            currentTheme = (Theme)InverseThemeBit;
+
+            RefreshThemeUI();
+            MessageBox.Show("Theme switched!\nA backup Unity.exe.bak file was created in the same directory.\nYou can restore your editor by renaming the backup to Unity.exe if needed.", "Unity Theme Switcher", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private bool IsUnityRunning()
+        {
+            Process[] pname = Process.GetProcessesByName("Unity.exe");
+            if (pname.Length == 0)
+                return false;
+            return true;
+        }
+
+        private void BackupExecutable()
+        {
+            string backupPath = exePath + ".BAK";
+            bool shouldOverwrite = File.Exists(backupPath);
+            File.Copy(exePath, exePath + ".BAK", shouldOverwrite);
+        }
+
+        // True will be light theme, false will be dark theme
+        // 1 light, 0 absence of light
+        private Theme GetCurrentTheme()
+        {
+            if (unityVersion.StartsWith("2019"))
+            {
+                string fileName = GenerateUniqueLocalFilename();
+
+                string cmd = App.Current.Properties["DDPath"] + " skip=16519510 count=1 if=\"" + exePath + "\" of=\"" + fileName + "\" bs=1";
+                Process p = CreateConsoleProcess(cmd);
+                p.Start();
+                p.WaitForExit();
+
+                cmd = App.Current.Properties["CatPath"] + " \"" + fileName + "\"";
+                p = CreateConsoleProcess(cmd, true);
+                p.Start();
+                p.WaitForExit();
+
+                File.Delete(fileName);
+
+                string stringVal = string.Empty;
+
+                // There will only be one line with one character
+                // 1 byte contains 1 character
+                while (!p.StandardOutput.EndOfStream)
+                {
+                    stringVal = p.StandardOutput.ReadLine();
+                }
+
+                if (stringVal == "u")
+                    return Theme.Dark;
+                else
+                    return Theme.Light;
+            }
+            else
+                return Theme.NotSupported;
+        }
+
+        private void RefreshThemeUI()
+        {
+            switch (currentTheme)
+            {
+                case Theme.NotSupported:
+                    MessageBox.Show("Your version of Unity currently isn't supported by this tool.", "Unity Theme Switcher", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                case Theme.Dark:
+                    ThemeBackground.Background = System.Windows.Media.Brushes.Black;
+                    break;
+                case Theme.Light:
+                    ThemeBackground.Background = System.Windows.Media.Brushes.White;
+                    break;
+                default:
+                    break;
+            }
+
+            Toggle.Content = string.Concat("Switch to ", System.Enum.GetName(typeof(Theme), InverseThemeBit));
+        }
+
+        private Theme InverseThemeBit
+        {
+            get
+            {
+                if (currentTheme == Theme.NotSupported)
+                    return Theme.NotSupported;
+                else if (currentTheme == Theme.Light)
+                    return Theme.Dark;
+                else
+                    return Theme.Light;
+            }
+        }
+
+        private string GenerateUniqueLocalFilename()
+        {
+            System.Random random = new System.Random();
+            int fileName = random.Next();
+            string path = Path.GetTempPath();
+
+            while (File.Exists(string.Concat(path, fileName)))
+            {
+                fileName = random.Next();
+            }
+
+            return string.Concat(path, fileName);
+        }
+
+        private Process CreateConsoleProcess(string command, bool redirectStandardOutput = false, bool enableRaisingEvents = false, System.EventHandler callback = null)
+        {
+            string baseCommand = "/C ";
+            string fullCommand = baseCommand + command;
+
+            Process proc = new Process();
+            proc.EnableRaisingEvents = enableRaisingEvents;
+
+            proc.StartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = fullCommand,
+                UseShellExecute = false,
+                RedirectStandardOutput = redirectStandardOutput,
+                CreateNoWindow = true
+            };
+
+            if (enableRaisingEvents && callback != null)
+            {
+                proc.Exited += callback;
+            }
+
+            return proc;
+        }
+    }
+}
